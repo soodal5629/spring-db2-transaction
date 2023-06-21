fd0cf45744018b2eeb4b36919c5206cffc440e03
chaelynjang <chaelynjang@lotte.net>
2023-06-21 10:45:44 +0900

    트랜잭션 내부/외부 호출 테스트(중요)

---
+++ src/test/java/hello/springtx/apply/TxLevelTest.java
@@ -0,0 +1,53 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+@Slf4j
+public class TxLevelTest {
+
+    @Autowired
+    LevelService levelService;
+
+    @Test
+    void orderTest() {
+        levelService.write();
+        levelService.read();
+    }
+    @TestConfiguration
+    static class TxLevelTestConfig {
+        @Bean
+        LevelService levelService() {
+            return new LevelService();
+        }
+    }
+
+    @Transactional(readOnly = true) // 읽기 전용 트랜잭션
+    static class LevelService {
+
+        @Transactional(readOnly = false) // readOnly: default == false
+        public void write() {
+            log.info("call write");
+            printTxInfo();
+        }
+
+        public void read() {
+            log.info("call read");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+}
