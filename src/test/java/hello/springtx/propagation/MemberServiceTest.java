243a10ced7a8bfc4e352d4e68ccc836469f5330c
jcl <chaelynjang@lotte.net>
2023-06-29 10:30:12 +0900

    독립된 트랜잭션 예외 하나만 터질 때 각각의 커밋/롤백 확인

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -34,4 +34,24 @@ class MemberServiceTest {
         Assertions.assertTrue(logRepository.find(username).isPresent());
 
     }
+
+    /**
+     * memberService    @Transactional: OFF
+     * memberRepository @Transactional: ON
+     * logRepository    @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        // given
+        String username = "로그예외_outerTxOff_fail";
+
+        // when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        // then: 모든 데이터가 정상 저장된다.
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+
+    }
 }
